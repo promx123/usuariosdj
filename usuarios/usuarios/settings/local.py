e930b7815fefa3adf675940236598c42b282ee2d
Facu <promx123@gmail.com>
2022-10-16 22:54:28 -0300

    configuracion del proyecto

---
+++ usuarios/usuarios/settings/local.py
@@ -0,0 +1,28 @@
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD':get_secret('PASSWORD'),
+        'HOST':'localhost',
+        'PORT':'5432',
+    }
+}
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+     BASE_DIR / "../static"
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / '../media'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
